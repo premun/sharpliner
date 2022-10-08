265f67b9bdd73fd0c3375fd541d6461bb7cd01ed
Premek Vysoky <premek.vysoky@microsoft.com>
2022-10-08 11:12:57 +0200

    Add a codeql workflow to the CI project

---
+++ tests/Sharpliner.Tests/GitHubActions/CodeQLAnalysisWorkflow.cs
@@ -0,0 +1,63 @@
+﻿using Sharpliner.GitHubActions;
+
+namespace Sharpliner.Tests.GitHubActions;
+
+class CodeQLAnalysisWorkflow : WorkflowDefinition
+{
+    public override string TargetFile => ".github/workflows/codeql-analysis.yml";
+
+    public override TargetPathType TargetPathType => TargetPathType.RelativeToGitRoot;
+
+    public override Workflow Workflow => new()
+    {
+        Name = "CodeQL",
+
+        On = new()
+        {
+            Push = new()
+            {
+                Branches = { "main" },
+            },
+            PullRequest = new()
+            {
+                Branches = { "main" },
+            },
+        },
+
+        Jobs =
+        {
+            new Job("Analyze")
+            {
+                RunsOn = "ubuntu-latest",
+
+                Steps =
+                {
+                    new()
+                    {
+
+                    },
+
+                    new Step("actions/setup-dotnet@v1")
+                    {
+                        With = new()
+                        {
+                            dotnetVersion = "5.0.x",
+                        },
+                    },
+
+                    new Step("github/codeql-action/init@v1")
+                    {
+                        With = new()
+                        {
+                            languages = "csharp",
+                        },
+                    },
+
+                    new Step("github/codeql-action/autobuild@v1"),
+
+                    new Step("github/codeql-action/analyze@v1"),
+                },
+            },
+        }
+    };
+}
