277af445d4c9681f3bf2703bf49c0ede8432a7eb
Premek Vysoky <premek.vysoky@microsoft.com>
2022-10-12 23:27:25 +0200

    Split logic into TemplateApiGenerator

--- tools/Sharpliner.Tools/Commands/TemplateApiCommand.cs
+++ tools/Sharpliner.Tools/Commands/TemplateApiCommand.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Sharpliner.Tools.CommandArguments;
-using YamlDotNet.Serialization;
-using YamlDotNet.Serialization.NamingConventions;
+using Sharpliner.Tools.TemplateApi;
 
 namespace Sharpliner.Tools.Commands;
 
@@ -15,16 +13,7 @@ internal class TemplateApiCommand : BaseCommand<TemplateApiCommandArguments>
 {
     private const string CommandHelp = "Parses an existing YAML template and creates C# API for referencing it";
 
-    private static string GetNewContent(string namespaceName, string className) =>
-        $$"""
-        using Sharpliner.AzureDevOps;
-
-        namespace {{namespaceName}};
-
-        public static class {{className}}
-        {
-        }
-        """;
+    private readonly TemplateApiGenerator _templateGenerator = new();
 
     protected override TemplateApiCommandArguments Arguments { get; } = new();
     protected override string CommandUsage { get; } = "template-api [OPTIONS] [TEMPLATE1.yml] [TEMPLATE2.yml] ...";
@@ -58,39 +47,13 @@ internal class TemplateApiCommand : BaseCommand<TemplateApiCommandArguments>
 
     private void ProcessTemplate(ILogger logger, string path)
     {
+        string? content = File.Exists(Arguments.OutputPath) ? File.ReadAllText(Arguments.OutputPath) : null;
+        string className = Path.GetFileNameWithoutExtension(path);
+
+        var lines = _templateGenerator.AddOrUpdateApi(Arguments.Namespace, className, content, path);
+
         var parentFolder = Path.GetDirectoryName(Arguments.OutputPath);
         Directory.CreateDirectory(parentFolder!);
-
-        var deserializer = new DeserializerBuilder()
-            .WithNamingConvention(CamelCaseNamingConvention.Instance)
-            .Build();
-
-        var template = deserializer.Deserialize<TemplateDefinition>(File.OpenText(path));
-
-        var content = File.Exists(Arguments.OutputPath)
-            ? File.ReadAllText(Arguments.OutputPath)
-            : GetNewContent(Arguments.Namespace, Path.GetFileNameWithoutExtension(path));
-
-        var newLine = content.Contains("\r\n") ? "\r\n" : Environment.NewLine;
-
-        var lines = content.Split(newLine);
-
-        File.WriteAllLines(path, lines);
-    }
-
-    private class TemplateDefinition
-    {
-        public List<TemplateParameter>? Parameters { get; set; }
-        public Dictionary<object, object>? Stages { get; set; }
-        public Dictionary<object, object>? Jobs { get; set; }
-        public Dictionary<object, object>? Steps { get; set; }
-        public Dictionary<object, object>? Variables { get; set; }
-    }
-
-    private class TemplateParameter
-    {
-        public string? Name { get; set; }
-        public string? Type { get; set; }
-        public object? Default { get; set; }
+        File.WriteAllLines(Arguments.OutputPath, lines);
     }
 }
---
+++ tools/Sharpliner.Tools/TemplateApi/TemplateApiGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace Sharpliner.Tools.TemplateApi;
+
+public class TemplateApiGenerator
+{
+    private static string GetNewContent(string namespaceName, string className) =>
+        $$"""
+        using Sharpliner.AzureDevOps;
+
+        namespace {{namespaceName}};
+
+        public static class {{className}}
+        {
+        }
+        """;
+
+    private static readonly IDeserializer s_deserializer = new DeserializerBuilder()
+        .WithNamingConvention(CamelCaseNamingConvention.Instance)
+        .Build();
+
+    public string[] AddOrUpdateApi(string namespaceName, string className, string? content, string templatePath)
+    {
+        TemplateDefinition template;
+        using (var reader = File.OpenText(templatePath))
+        {
+            template = s_deserializer.Deserialize<TemplateDefinition>(reader);
+        }
+
+        content ??= GetNewContent(namespaceName, className);
+
+        var newLine = content.Contains("\r\n") ? "\r\n" : Environment.NewLine;
+        var lines = content.Split(newLine).ToList();
+
+        return lines.ToArray();
+    }
+
+    class TemplateDefinition
+    {
+        public Dictionary<string, object>? Parameters { get; set; }
+        public List<Dictionary<object, object>>? Stages { get; set; }
+        public List<Dictionary<object, object>>? Jobs { get; set; }
+        public List<Dictionary<object, object>>? Steps { get; set; }
+        public List<Dictionary<object, object>>? Variables { get; set; }
+    }
+
+    private class TemplateParameter
+    {
+        public string? Name { get; set; }
+        public string? Type { get; set; }
+        public object? Default { get; set; }
+    }
+}
